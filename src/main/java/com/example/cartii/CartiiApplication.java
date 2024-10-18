6bf3a55434c014b2ee59bf020a156256f86b175e
sumane <sumangq5@gmail.com>
2024-10-17 23:45:34 -0400

    version final

---
+++ src/main/java/com/example/cartii/CartiiApplication.java
@@ -0,0 +1,13 @@
+package com.example.cartii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartiiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/cartii/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.example.cartii;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    // Add any custom queries if needed, e.g., finding by name
+    Product findByProductName(String productName);
+}
